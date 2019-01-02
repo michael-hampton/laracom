beb304f1649950ee8ef204829efc5679353b7262
michael-hampton <michael.hampton@kondor.co.uk>
2019-01-02 21:39:51 +0000

    trying out toggles

--- resources/views/admin/channels/edit.blade.php
+++ resources/views/admin/channels/edit.blade.php
@@ -1,5 +1,104 @@
 @extends('layouts.admin.app')
 
+<?php
+
+function buildcheckBox($value, $label) {
+
+    $checked = $value == 1 ? 'checked' : '';
+
+    echo '<div class="switch">
+    <div class="onoffswitch">
+        <input type="checkbox" ' . $checked . ' class="onoffswitch-checkbox" id="' . $label . '">
+        <label class="onoffswitch-label" for="' . $label . '">
+            <span class="onoffswitch-inner"></span>
+            <span class="onoffswitch-switch"></span>
+        </label>
+    </div>
+</div>';
+}
+?>
+
+<style>
+    /* SWITCHES */
+    .onoffswitch {
+        position: relative;
+        width: 54px;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+    }
+    .onoffswitch-checkbox {
+        display: none;
+    }
+    .onoffswitch-label {
+        display: block;
+        overflow: hidden;
+        cursor: pointer;
+        border: 2px solid #1AB394;
+        border-radius: 3px;
+    }
+    .onoffswitch-inner {
+        display: block;
+        width: 200%;
+        margin-left: -100%;
+        -moz-transition: margin 0.3s ease-in 0s;
+        -webkit-transition: margin 0.3s ease-in 0s;
+        -o-transition: margin 0.3s ease-in 0s;
+        transition: margin 0.3s ease-in 0s;
+    }
+    .onoffswitch-inner:before,
+    .onoffswitch-inner:after {
+        display: block;
+        float: left;
+        width: 50%;
+        height: 16px;
+        padding: 0;
+        line-height: 16px;
+        font-size: 10px;
+        color: white;
+        font-family: Trebuchet, Arial, sans-serif;
+        font-weight: bold;
+        -moz-box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+    .onoffswitch-inner:before {
+        content: "ON";
+        padding-left: 7px;
+        background-color: #1AB394;
+        color: #FFFFFF;
+    }
+    .onoffswitch-inner:after {
+        content: "OFF";
+        padding-right: 7px;
+        background-color: #FFFFFF;
+        color: #919191;
+        text-align: right;
+    }
+    .onoffswitch-switch {
+        display: block;
+        width: 18px;
+        margin: 0;
+        background: #FFFFFF;
+        border: 2px solid #1AB394;
+        border-radius: 3px;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 36px;
+        -moz-transition: all 0.3s ease-in 0s;
+        -webkit-transition: all 0.3s ease-in 0s;
+        -o-transition: all 0.3s ease-in 0s;
+        transition: all 0.3s ease-in 0s;
+    }
+    .onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
+        margin-left: 0;
+    }
+    .onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
+        right: 0;
+    }
+</style>
+
 @section('content')
 <!-- Main content -->
 <section class="content">
@@ -35,13 +134,15 @@
                             <input type="file" name="cover" id="cover" class="form-control">
                         </div>
 
-                        <div class="form-group">
-                            <label for="status">Has Priority </label>
-                            <select name="has_priority" id="has_priority" class="form-control">
-                                <option value="1" {{ $channel->has_priority === 1 ? 'selected="selected"' : '' }}>Yes</option>
-                                <option value="0" {{ $channel->has_priority === 0 ? 'selected="selected"' : '' }}>No</option>
-                            </select>
-                        </div>
+                        {{buildCheckbox($channel->has_priority, 'has_priority')}}
+
+                        <!--                        <div class="form-group">
+                                                    <label for="status">Has Priority </label>
+                                                    <select name="has_priority" id="has_priority" class="form-control">
+                                                        <option value="1" {{ $channel->has_priority === 1 ? 'selected="selected"' : '' }}>Yes</option>
+                                                        <option value="0" {{ $channel->has_priority === 0 ? 'selected="selected"' : '' }}>No</option>
+                                                    </select>
+                                                </div>-->
 
                         <div class="form-group">
                             <label for="status">Allocate On Order </label>
@@ -73,6 +174,30 @@
                         </div>
 
                         <div class="form-group">
+                            <label for="status">Send Dispatched Email </label>
+                            <select name="send_dispatched_email" id="send_dispatched_email" class="form-control">
+                                <option value="1" {{ $channel->send_dispatched_email === 1 ? 'selected="selected"' : '' }}>Yes</option>
+                                <option value="0" {{ $channel->send_dispatched_email === 0 ? 'selected="selected"' : '' }}>No</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="status">Strict validation </label>
+                            <select name="send_dispatched_email" id="send_dispatched_email" class="form-control">
+                                <option value="1" {{ $channel->send_dispatched_email === 1 ? 'selected="selected"' : '' }}>Yes</option>
+                                <option value="0" {{ $channel->send_dispatched_email === 0 ? 'selected="selected"' : '' }}>No</option>
+                            </select>
+                        </div>
+                        
+                         <div class="form-group">
+                            <label for="status">Partial Shipment </label>
+                            <select name="send_dispatched_email" id="send_dispatched_email" class="form-control">
+                                <option value="1" {{ $channel->send_dispatched_email === 1 ? 'selected="selected"' : '' }}>Yes</option>
+                                <option value="0" {{ $channel->send_dispatched_email === 0 ? 'selected="selected"' : '' }}>No</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
                             <label for="status">Status </label>
                             <select name="status" id="status" class="form-control">
                                 <option value="0" @if($channel->status == 0) selected="selected" @endif>Disable</option>
@@ -95,5 +220,21 @@
     <!-- /.box -->
 
 </section>
+
+
+
 <!-- /.content -->
 @endsection
+
+@section('js')
+<script type="text/javascript">
+    $(document).ready(function () {
+        
+        $(".onoffswitch-inner").off();
+         $(".onoffswitch-inner").click(function() {
+             alert('Mike');
+         });
+    });
+</script>
+@endsection
+
