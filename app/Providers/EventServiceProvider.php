b11bbf2f2283a747700812df2ebc839e2674e4d8
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-03 14:26:07 +0000

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,9 @@ class EventServiceProvider extends ServiceProvider {
         'App\Events\OrderCreateEvent' => [
             'App\Listeners\OrderCreateEventListener',
         ],
+        'App\Events\RefundCreateEvent' => [
+            'App\Listeners\RefundCreateEventListener',
+        ],
     ];
 
     /**
