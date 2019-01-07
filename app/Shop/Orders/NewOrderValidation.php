c34494c5c20c8eaafe0817fbf286742f3e1d2d70
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-07 11:56:54 +0000

    Update NewOrderValidation.php

--- app/Shop/Orders/NewOrderValidation.php
+++ app/Shop/Orders/NewOrderValidation.php
@@ -20,5 +20,43 @@ $customerRepo->findCustomerById($id);
 return false;
 }
 }
+  
+  private function validateCustomerRef($customerRef) {
+
+        if (strlen($customerRef) > 36) {
+            return false;
+        }
+
+        try {
+            $result = $this->listOrders()->where('customer_ref', $customerRef);
+        } catch (Exception $ex) {
+            throw new Exception($ex->getMessage());
+        }
+
+        return $result->isEmpty();
+    }
+
+    private function validateTotal($data, $cartItems) {
+        $productTotal = 0;
+
+        foreach ($cartItems as $cartItem) {
+
+            $productTotal += $cartItem->price;
+        }
+
+        $total = $productTotal + $data['shipping'] + $data['tax'];
+
+        if (!empty($data['discounts']) && $data['discounts'] > 0) {
+            $total -= $data['discounts'];
+        }
+
+
+        if (round($total, 2) !== round($data['total'], 2)) {
+
+            return false;
+        }
+
+        return true;
+    }
 
 }
