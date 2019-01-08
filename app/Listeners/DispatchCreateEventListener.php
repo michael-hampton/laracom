4a0d551246e768eed1eb573593ba4ecccb135a83
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-08 14:12:41 +0000

    Create DispatchCreateEventListener.php

---
+++ app/Listeners/DispatchCreateEventListener.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Listeners;
+use App\Events\DispatchCreateEvent;
+use App\Shop\Orders\Repositories\OrderRepository;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+class DispatchCreateEventListener {
+    /**
+     * Create the event listener.
+     *
+     */
+    public function __construct() {
+        //
+    }
+    /**
+     * Handle the event.
+     *
+     * @param  DispatchCreateEvent  $event
+     * @return void
+     */
+    public function handle(DispatchCreateEvent $event) {
+                       
+        // send email to customer
+        $orderRepo = new OrderRepository($event->order);
+        $orderRepo->sendDispatchEmail();
+                
+        $orderRepo = new OrderRepository($event->order);
+        $orderRepo->sendDispatchEmailNotificationToAdmin();        
+    }
+}
