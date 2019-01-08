9bac69d6213bfa5a875ace843da799d124d5b792
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-08 12:53:22 +0000

    Update CourierRateRepository.php

--- app/Shop/Couriers/CourierRateRepository.php
+++ app/Shop/Couriers/CourierRateRepository.php
@@ -1,40 +1,41 @@
 <?php
-namespace App\Shop\Countries\Repositories;
+namespace App\Shop\CourierRates\Repositories;
 use App\Shop\Base\BaseRepository;
-use App\Shop\Countries\Exceptions\CountryInvalidArgumentException;
-use App\Shop\Countries\Exceptions\CountryNotFoundException;
-use App\Shop\Countries\Repositories\Interfaces\CountryRepositoryInterface;
+use App\Shop\CourierRates\Exceptions\CourierRateInvalidArgumentException;
+use App\Shop\CourierRates\Exceptions\CourierRateNotFoundException;
+use App\Shop\CourierRates\Repositories\Interfaces\CourierRateRepositoryInterface;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
-use App\Shop\Countries\Country;
-class CountryRepository extends BaseRepository implements CountryRepositoryInterface
+use App\Shop\CourierRates\CourierRate;
+
+class CourierRateRepository extends BaseRepository implements CountryRepositoryInterface
 {
     /**
      * CountryRepository constructor.
      * @param Country $country
      */
-    public function __construct(Country $country)
+    public function __construct(CourierRate $courierRate)
     {
-        parent::__construct($country);
-        $this->model = $country;
+        parent::__construct($courierRate);
+        $this->model = $courierRate;
     }
     /**
-     * List all the countries
+     * List all the courier rates
      *
      * @param string $order
      * @param string $sort
      * @return Collection
      */
-    public function listCountries(string $order = 'id', string $sort = 'desc') : Collection
+    public function listCourierRates(string $order = 'id', string $sort = 'desc') : Collection
     {
-        return $this->model->where('status', 1)->get();
+        return $this->model->get();
     }
     /**
      * @param array $params
      * @return Country
      */
-    public function createCountry(array $params) : Country
+    public function createCourierRate(array $params) : CourierRate
     {
         return $this->create($params);
     }
@@ -45,34 +46,26 @@ class CountryRepository extends BaseRepository implements CountryRepositoryInter
      * @return Country
      * @throws CountryNotFoundException
      */
-    public function findCountryById(int $id) : Country
+    public function findCourierRateById(int $id) : CourierRate
     {
         try {
             return $this->findOneOrFail($id);
         } catch (ModelNotFoundException $e) {
-            throw new CountryNotFoundException($e->getMessage());
+            throw new CourierRateNotFoundException($e->getMessage());
         }
     }
+    
     /**
-     * Show all the provinces
-     *
-     * @return mixed
-     */
-    public function findProvinces()
-    {
-        return $this->model->provinces;
-    }
-    /**
-     * Update the country
+     * Update the courier rate
      *
      * @param array $params
-     * @return Country
+     * @return CourierRate
      */
-    public function updateCountry(array $params) : Country
+    public function updateCourierRate(array $params) : CourierRate
     {
         try {
             $this->model->update($params);
-            return $this->findCountryById($this->model->id);
+            return $this->findCourierRateById($this->model->id);
         } catch (QueryException $e) {
             throw new CountryInvalidArgumentException($e->getMessage());
         }
