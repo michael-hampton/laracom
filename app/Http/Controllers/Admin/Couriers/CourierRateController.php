60053c940c4df4f0c7c1416c89cc35b1b031e20b
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-08 12:20:53 +0000

    Create CourierRateController.php

---
+++ app/Http/Controllers/Admin/Couriers/CourierRateController.php
@@ -0,0 +1,90 @@
+namespace App\Http\Controllers\Admin\Couriers;
+use App\Shop\Couriers\Repositories\CourierRepository;
+use App\Shop\Couriers\Repositories\Interfaces\CourierRepositoryInterface;
+use App\Shop\Couriers\Requests\CreateCourierRequest;
+use App\Shop\Couriers\Requests\UpdateCourierRequest;
+use App\Shop\Countries\Repositories\CountryRepository;
+use App\Shop\Countries\Country;
+use App\Http\Controllers\Controller;
+<?php
+class CourierRateController extends Controller {
+    /**
+     * @var CourierRepositoryInterface
+     */
+    private $courierRepo;
+    /**
+     * CourierController constructor.
+     * @param CourierRepositoryInterface $courierRepository
+     */
+    public function __construct(CourierRateRepositoryInterface $courierRepository) {
+        $this->courierRepo = $courierRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        return view('admin.couriers.list', ['couriers' => $this->courierRepo->listCouriers('name', 'asc')]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        $countryRepo = new CountryRepository(new Country);
+        $countries = $countryRepo->listCountries();
+        return view('admin.couriers.create', ['countries' => $countries]);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CreateCourierRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateCourierRequest $request) {
+        $this->courierRepo->createCourier($request->all());
+        $request->session()->flash('message', 'Create successful');
+        return redirect()->route('admin.couriers.index');
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(int $id) {
+        $countryRepo = new CountryRepository(new Country);
+        $countries = $countryRepo->listCountries();
+        return view('admin.couriers.edit', ['courier' => $this->courierRepo->findCourierById($id), 'countries' => $countries]);
+    }]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  UpdateCourierRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCourierRequest $request, $id) {
+        $courier = $this->courierRepo->findCourierById($id);
+        $update = new CourierRepository($courier);
+        $update->updateCourier($request->all());
+        $request->session()->flash('message', 'Update successful');
+        return redirect()->route('admin.couriers.edit', $id);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id) {
+        $courier = $this->courierRepo->findCourierById($id);
+        $courierRepo = new CourierRepository($courier);
+        $courierRepo->delete();
+        request()->session()->flash('message', 'Delete successful');
+        return redirect()->route('admin.couriers.index');
+    }
+}
