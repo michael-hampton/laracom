210fe30275d970e47c656cb08a7c2402c8c91089
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-12 11:15:28 +0000

    Create SearchableTrait.php

---
+++ app/Traits/SearchableTrait.php
@@ -0,0 +1,33 @@
+<?php
+trait SearchableTrait {
+protected static function applyDecoratorsFromRequest(Request $request, Builder $query)
+    {
+        foreach ($request->all() as $filterName => $value) {
+
+            $decorator = static::createFilterDecorator($filterName);
+
+            if (static::isValidDecorator($decorator)) {
+                $query = $decorator::apply($query, $value);
+            }
+
+        }
+        return $query;
+    }
+    
+    protected static function createFilterDecorator($name)
+    {
+        return return __NAMESPACE__ . '\\Filters\\' . 
+            str_replace(' ', '', 
+                ucwords(str_replace('_', ' ', $name)));
+    }
+    
+    protected static function isValidDecorator($decorator)
+    {
+        return class_exists($decorator);
+    }
+
+    protected static function getResults(Builder $query)
+    {
+        return $query->get();
+    }
+}
