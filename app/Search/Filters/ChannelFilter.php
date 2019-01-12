18a26ea0727349cdff2a1bee33baef782155ea27
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-12 11:24:31 +0000

    Create ChannelFilter.php

---
+++ app/Search/Filters/ChannelFilter.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Database\Eloquent\Builder;
+
+class Channel
+{
+
+    /**
+     * Apply a given search value to the builder instance.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder $builder
+     */
+    public static function apply(Builder $builder, $value)
+    {
+        return $builder->where('channel', $value);
+    }
+}
