79371b8cfdb3b48bfd892c9d77c0f0a1aef92daa
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-12 12:18:12 +0000

    Update list.blade.php

--- resources/views/admin/channel-price/list.blade.php
+++ resources/views/admin/channel-price/list.blade.php
@@ -9,7 +9,43 @@
             <div class="box">
                 <div class="box-body">
                     <h2>Products</h2>
-                    @include('layouts.search', ['route' => route('admin.products.index')])
+                    
+                    <form action="{{ route('admin.courier-rates.search' }}" method="post" id="admin-search">
+    
+            <div class="form-group">
+                    <label for="channel">Channel</label>
+                    <select name="channel" id="channel" class="form-control select2">
+                        <option value="">--Select--</option>
+                        @foreach($channels as $channel)
+                        <option @if(old('channel') == $channel->id) selected="selected" @endif value="{{ $channel->id }}">{{ $channel->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    <label for="country">Category </label>
+                    <select name="category" id="category" class="form-control">
+                        @foreach($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                
+                <div class="form-group">
+                    <label for="country">Brand </label>
+                    <select name="brand" id="brand" class="form-control">
+                        @foreach($brands as $brand)
+                        <option value="{{ $brand->id }}">{{ $brand->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                
+                <div class="form-group">
+                    <label for="product_name">Product Name</label>
+                    <input name="product_name" id="product_name" class="form-control">
+                </div>
+                </form>
+                    
                     @include('admin.shared.channel-products')
                     {{ $products->links() }}
                 </div>
