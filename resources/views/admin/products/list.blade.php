83f2aea2214eb348193d46e131173d699229f44e
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-12 12:20:11 +0000

    Update list.blade.php

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -9,8 +9,7 @@
             <div class="box">
                 <div class="box-body">
                     <h2>Products</h2>
-                    <form action="{{ route('admin.products
-                    .search' }}" method="post" id="admin-search">
+                    <form action="{{ route('admin.products.search' }}" method="post" id="admin-search">
     
             <div class="form-group">
                     <label for="channel">Channel</label>
