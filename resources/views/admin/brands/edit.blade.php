c5e2b4488a6dad030ef82fee46f4e54e1e410ab7
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-14 09:41:19 +0000

    Update edit.blade.php

--- resources/views/admin/brands/edit.blade.php
+++ resources/views/admin/brands/edit.blade.php
@@ -9,9 +9,29 @@
                 <div class="box-body">
                     {{ csrf_field() }}
                     <input type="hidden" name="_method" value="put">
+                    
                     <div class="form-group">
                         <label for="name">Name <span class="text-danger">*</span></label>
                         <input type="text" name="name" id="name" placeholder="Name" class="form-control" value="{{ $brand->name }}">
+                    </div>
+                    
+                    @if(isset($brand->cover))
+                    <div class="form-group">
+                        <img src="{{ asset("storage/$brand->cover") }}" alt="" class="img-responsive"> <br/>
+                        <a onclick="return confirm('Are you sure?')" href="{{ route('admin.brand.remove.image', ['brand' => $brand->id]) }}" class="btn btn-danger">Remove image?</a>
+                    </div>
+                    @endif
+                    
+                    <div class="form-group">
+                        <label for="cover">Cover </label>
+                        <input type="file" name="cover" id="cover" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="status">Status </label>
+                        <select name="status" id="status" class="form-control">
+                            <option value="0" @if($brand->status == 0) selected="selected" @endif>Disable</option>
+                            <option value="1" @if($brand->status == 1) selected="selected" @endif>Enable</option>
+                        </select>
                     </div>
                 </div>
                 <!-- /.box-body -->
