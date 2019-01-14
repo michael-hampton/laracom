fedd2b4b894cf08df3e84c8a538fbd987c7d7fe5
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-14 10:33:34 +0000

    Update Brand.php

--- app/Shop/Brands/Brand.php
+++ app/Shop/Brands/Brand.php
@@ -4,9 +4,22 @@ namespace App\Shop\Brands;
 
 use App\Shop\Products\Product;
 use Illuminate\Database\Eloquent\Model;
-
+use Nicolaslopezj\Searchable\SearchableTrait;
 class Brand extends Model
 {
+    use SearchableTrait;
+ 
+    /**
+     * Searchable rules.
+     *
+     * @var array
+     */
+    protected $searchable = [
+        'columns' => [
+            'brands.name' => 10
+        ]
+    ];
+    
     protected $fillable = [
         'name',
         'cover',
