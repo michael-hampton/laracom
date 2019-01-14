86f1a231c9554c04cfc1378b25fcc9dc1daf2d7d
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-14 14:13:13 +0000

    Create ProductStatus.php

---
+++ app/Search/Filters/ProductStatus.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Search\Filters;
+use Illuminate\Database\Eloquent\Builder;
+class ProductStatus {
+    /**
+     * Apply a given search value to the builder instance.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder $builder
+     */
+    public static function apply(Builder $builder, $value) {
+                
+        return $builder->where('order_status_id', $value);
+    }
+}
