8aa0d97dd92ab36e1b43f269986eaa27329a3d67
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-17 13:37:07 +0000

    Update backorders.blade.php

--- resources/views/admin/orders/backorders.blade.php
+++ resources/views/admin/orders/backorders.blade.php
@@ -198,10 +198,13 @@ function getInventoryForProduct($productId, $arrProducts) {
                 <small class="font-bold">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</small>
             </div>
             <div class="modal-body">
-                <p><strong>Lorem Ipsum is simply dummy</strong> text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-                    remaining essentially unchanged.</p>
-                <div class="form-group"><label>Sample Input</label> <input type="email" placeholder="Enter your email" class="form-control"></div>
+            
+           
+                
+                <div class="form-group">
+                <label>Comment</label> 
+                <textarea id='comment' name='comment' class='form-control'></textarea>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
