3fc0f6d8e34846d1ab934da0b0e4c1b466862db9
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-17 22:47:05 +0000

    Create ReturnLine.php

---
+++ app/Shop/Returns/ReturnLine.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Shop\Returns;
+use App\Shop\Orders\Order;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Sofa\Eloquence\Eloquence;
+class ReturnLine extends Model {
+    use SoftDeletes,
+        Eloquence;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = [
+        'order_id',
+        'line_id',
+        'quantity',
+        'amount',
+        'date_refunded',
+        'status'
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+    
+    /**
+     *
+     * @var type 
+     */
+    protected $dates = ['deleted_at', 'date_returned'];
+    /**
+     * 
+     * @return type
+     */
+    public function order() {
+        return $this->belongsTo(Order::class);
+    }
+}
