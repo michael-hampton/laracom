9c3d0bc75d271d624e7e045af05b2beb3cd51a2a
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-17 22:51:21 +0000

    Create ReturnInterface.php

---
+++ app/Shop/Returns/Repositories/Interfaces/ReturnInterface.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Shop\Returns\Repositories\Interfaces;
+use App\Shop\Returns\Return;
+use App\Shop\Base\Interfaces\BaseRepositoryInterface;
+use App\Shop\Orders\Order;
+use Illuminate\Support\Collection;
+interface RefundRepositoryInterface extends BaseRepositoryInterface {
+    /**
+     * 
+     * @param array $params
+     */
+    public function createReturn(array $params): Return;
+    /**
+     * 
+     * @param array $update
+     */
+    public function updateReturn(array $update): bool;
+    /**
+     * 
+     */
+    public function deleteReturn();
+    /**
+     * 
+     * @param string $order
+     * @param string $sort
+     * @param array $columns
+     */
+    public function listReturn(string $order = 'id', string $sort = 'desc', array $columns = ['*']): Collection;
+    /**
+     * 
+     * @param int $id
+     */
+    public function findReturnById(int $id): Return;
+    /**
+     * 
+     * @param string $text
+     */
+    public function searchReturn(string $text): Collection;
+}
