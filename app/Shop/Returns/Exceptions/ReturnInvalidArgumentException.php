1f7ed2396e03c10ad29b2c519f824f9f19f5fde1
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-17 21:17:42 +0000

    Create ReturnInvalidArgumentException.php

---
+++ app/Shop/Returns/Exceptions/ReturnInvalidArgumentException.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Shop\Returns\Exceptions;
+use Doctrine\Instantiator\Exception\InvalidArgumentException;
+class ReturnInvalidArgumentException extends InvalidArgumentException
+{
+}
