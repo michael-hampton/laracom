c77f120f90ab520da957b4d943930bfd6c03f4dc
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-18 18:37:58 +0000

    Update list.blade.php

--- resources/views/admin/courier-rates/list.blade.php
+++ resources/views/admin/courier-rates/list.blade.php
@@ -35,6 +35,71 @@
         </div>
 
     </form>
+    
+    <div class="box">
+        <form action="{{ route('admin.courier-rates.store') }}" method="post" class="form">
+            <div class="box-body">
+                {{ csrf_field() }}
+                
+                <div class='inline-form'>
+                <div class="form-group col-lg-3">
+                    <label for="courier">Courier</label>
+                    <select name="courier" id="courier" class="form-control select2">
+                        <option value="">--Select--</option>
+                        @foreach($couriers as $courier)
+                        <option @if(old('courier') == $courier->id) selected="selected" @endif value="{{ $courier->id }}">{{ $courier->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <!-- <div class="form-group">
+                    <label class='sr-only' for="channel">Channel</label>
+                    <select name="channel" id="channel" class="form-control select2">
+                        <option value="">--Select--</option>
+                        @foreach($channels as $channel)
+                        <option @if(old('channel') == $channel->id) selected="selected" @endif value="{{ $channel->id }}">{{ $channel->name }}</option>
+                        @endforeach
+                    </select>
+                </div> -->
+
+                <div class="form-group col-lg-2">
+                    <label class='sr-only' for="range_from">Range From</label>
+                    <div class="input-group">
+                        <input type="text" name="range_from" id="range_from" placeholder="Range From" class="form-control" value="{{ old('range_from') }}">
+                    </div>
+                </div>
+
+                <div class="form-group col-lg-2">
+                    <label class='sr-only' for="range_to">Range To</label>
+                    <div class="input-group">
+                        <input type="text" name="range_to" id="range_to" placeholder="Range To" class="form-control" value="{{ old('range_to') }}">
+                    </div>
+                </div>
+
+                <div class="form-group col-lg-2">
+                    <label class='sr-only' for="cost">Cost</label>
+                    <div class="input-group">
+                        <input type="text" name="cost" id="cost" placeholder="Cost" class="form-control" value="{{ old('cost') }}">
+                    </div>
+                </div>
+
+                <div class="form-group col-lg-3">
+                    <label class='sr-only' for="country">Country </label>
+                    <select name="country" id="country" class="form-control">
+                        @foreach($countries as $country)
+                        <option value="{{ $country->id }}">{{ $country->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <div class="btn-group">
+                    <a href="{{ route('admin.courier-rates.index') }}" class="btn btn-default">Back</a>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </div>
+            </div>
 
     <!-- Default box -->
     @if($couriers)
