29d99296eb35cf9ca62e1492ae19baf5df2dc740
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-19 20:04:03 +0000

    Update search.blade.php

--- resources/views/admin/courier-rates/search.blade.php
+++ resources/views/admin/courier-rates/search.blade.php
@@ -3,45 +3,49 @@
 <div class="box">
     <div class="box-body">
         <h2> <i class="fa fa-truck"></i> Couriers</h2>
-        <table class="table">
-            <thead>
-                <tr>
-                    <td class="col-md-2">Courier</td>
-                    <td class="col-md-2">Country</td>
-                    <td class="col-md-2">Channel</td>
-                    <td class="col-md-2">From</td>
-                    <td class="col-md-1">To</td>
-                    <td class="col-md-1">Cost</td>
-                    <td class="col-md-3">Actions</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($courier_rates as $courier)
-                <tr>
-                    <td>{{ $couriers[$courier->courier]->name }}</td>
-                    <td>{{ $courier->country }}</td>
-                    <td>{{ $courier->channel_name }}</td>
-                    <td>{{ $courier->range_from }}</td>
-                    <td>{{ $courier->range_to }}</td>
-                    <td>{{ $courier->cost }}</td>
-<!--                                <td>
-                        @include('layouts.status', ['status' => $courier->is_free])
-                    </td>-->
-                    <!-- <td>@include('layouts.status', ['status' => $courier->status])</td> -->
-                    <td>
-                        <form action="{{ route('admin.courier-rates.destroy', $courier->id) }}" method="post" class="form-horizontal">
-                            {{ csrf_field() }}
-                            <input type="hidden" name="_method" value="delete">
-                            <div class="btn-group">
-                                <a href="{{ route('admin.couriers.edit', $courier->id) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> Edit</a>
-                                <button onclick="return confirm('Are you sure?')" type="submit" class="btn btn-danger btn-sm"><i class="fa fa-times"></i> Delete</button>
-                            </div>
-                        </form>
-                    </td>
-                </tr>
+            
+                @foreach ($courier_rates as $courier_rate)
+                <div class='inline-form'>
+                    <div class="form-group col-lg-3">
+                        <label for="rates[$courier_rate->id][courier]" class="sr-only">Courier</label>
+                        <select name="courier" id="courier" class="form-control select2">
+                            <option value="">--Select--</option>
+                            @foreach($couriers as $courier)
+                            <option @if($courier_rate->courier == $courier->id) selected="selected" @endif value="{{ $courier->id }}">{{ $courier->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group col-lg-2">
+                        <label class='sr-only' for="range_from">Range From</label>
+                        <div class="input-group">
+                            <input type="text" name="rates[$courier_rate->id][range_from]" id="range_from" placeholder="Range From" class="form-control" value="{{ $courier_rate->range_from }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group col-lg-2">
+                        <label class='sr-only' for="range_to">Range To</label>
+                        <div class="input-group">
+                            <input type="text" name="rates[$courier_rate->id][range_to]" id="range_to" placeholder="Range To" class="form-control" value="{{ $courier_rate->range_to }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group col-lg-2">
+                        <label class='sr-only' for="cost">Cost</label>
+                        <div class="input-group">
+                            <input type="text" name="rates[$courier_rate->id][cost]" id="cost" placeholder="Cost" class="form-control" value="{{ $courier_rate->cost }}">
+                        </div>
+                    </div>
+
+                    <div class="form-group col-lg-3">
+                        <label class='sr-only' for="country">Country </label>
+                        <select name="rates[$courier_rate->id][country]" id="country" class="form-control">
+                            @foreach($countries as $country)
+                            <option @if($courier_rate->country == $country->id) selected='selected' @endif value="{{ $country->id }}">{{ $country->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 @endforeach
-            </tbody>
-        </table>
     </div>
     <!-- /.box-body -->
 </div>
