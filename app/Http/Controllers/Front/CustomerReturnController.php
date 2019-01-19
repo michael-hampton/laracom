54d46f27adf1a7d18cdb7a9a80d2250c24d71275
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-19 19:10:54 +0000

    Update CustomerReturnController.php

--- app/Http/Controllers/Front/CustomerReturnController.php
+++ app/Http/Controllers/Front/CustomerReturnController.php
@@ -5,13 +5,90 @@ use App\Shop\Categories\Repositories\Interfaces\CategoryRepositoryInterface;
 use App\Http\Controllers\Controller;
 class CustomerReturnController extends Controller
 {
-    /**
-     * @var ReturnRepositoryInterface
-     */
+    use ReturnTransformable;
+    
+    /* @param ReturnRepositoryInterface $refundRepo */
     private $returnRepo;
     
-    /**
-     * @var ReturnLineRepositoryInterface
-     */
+    /* @param ReturnRepositoryInterface $refundRepo */
     private $returnLineRepo;
+    
+    /* @param OrderRepositoryInterface $orderRepo */
+    private $orderRepo;
+    
+    /**
+     * @var OrderStatusRepositoryInterface
+     */
+    private $orderStatusRepo;
+    
+    /**
+     * @var OrderProductRepositoryInterface
+     */
+    private $orderProductRepo;
+    
+    /**
+     * 
+     * @param ReturnRepositoryInterface $refundRepository
+     * @param OrderRepositoryInterface $orderRepository
+     */
+    public function __construct(
+    ReturnRepositoryInterface $returnRepository, ReturnLineRepositoryInterface $returnLineRepository, OrderRepositoryInterface $orderRepository, OrderStatusRepositoryInterface $orderStatusRepository, OrderProductRepositoryInterface $orderProductRepository
+    ) {
+        $this->returnRepo = $returnRepository;
+        $this->returnLineRepo = $returnLineRepository;
+        $this->orderRepo = $orderRepository;
+        $this->orderStatusRepo = $orderStatusRepository;
+        $this->orderProductRepo = $orderProductRepository;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $list = $this->returnRepo->listReturn('created_at', 'desc');
+        if (request()->has('q')) {
+            $list = $this->returnRepo->searchReturn(request()->input('q'));
+        }
+        $returns = $list->map(function (Returns $return) {
+                    return $this->transformReturn($return);
+                })->all();
+        return view('admin.returns.list', ['returns' => $this->returnRepo->paginateArrayResults($returns)]);
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($orderId) {
+        $order = $this->orderRepo->findOrderById($orderId);
+        $orderRepo = new OrderRepository($order);
+        $items = $orderRepo->listOrderedProducts();
+        $status = (new \App\Shop\Returns\ReturnStatus())->get();
+        return view('admin.returns.create', [
+            'order' => $order,
+            'items' => $items,
+            'reasons' => explode(',', env('RETURN_REASON')),
+            'statuses' => $status,
+            'conditions' => explode(',', env('RETURN_CONDITIONS')),
+            'resolutions' => explode(',', env('RETURN_RESOLUTIONS'))
+        ]);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  CreateReturnRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateReturnRequest $request) {
+        $data = $request->except('_token', '_method', 'lines');
+        $return = $this->returnRepo->createReturn($data);
+        foreach ($request->lines as $line) {
+            $this->returnLineRepo->createReturnLine($line, $return);
+        }
+        $request->session()->flash('message', 'Creation successful');
+        return redirect()->route('admin.returns.index');
+    }
 }
