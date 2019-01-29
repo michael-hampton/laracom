c5b955cec70bd74c75a63caec51636f41c341352
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-29 12:52:50 +0000

    Update create.blade.php

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -10,38 +10,54 @@
                 {{ csrf_field() }}
                 <div class="col-md-8">
                     <h2>Product</h2>
+                    
                     <div class="form-group">
                         <label for="sku">SKU <span class="text-danger">*</span></label>
                         <input type="text" name="sku" id="sku" placeholder="xxxxx" class="form-control" value="{{ old('sku') }}">
                     </div>
+                    
                     <div class="form-group">
                         <label for="name">Name <span class="text-danger">*</span></label>
                         <input type="text" name="name" id="name" placeholder="Name" class="form-control" value="{{ old('name') }}">
                     </div>
+                    
                     <div class="form-group">
                         <label for="description">Description </label>
                         <textarea class="form-control" name="description" id="description" rows="5" placeholder="Description">{{ old('description') }}</textarea>
                     </div>
+                    
                     <div class="form-group">
                         <label for="cover">Cover </label>
                         <input type="file" name="cover" id="cover" class="form-control">
                     </div>
+                    
                     <div class="form-group">
                         <label for="image">Images</label>
                         <input type="file" name="image[]" id="image" class="form-control" multiple>
                         <small class="text-warning">You can use ctr (cmd) to select multiple images</small>
                     </div>
+                    
                     <div class="form-group">
                         <label for="quantity">Quantity <span class="text-danger">*</span></label>
                         <input type="text" name="quantity" id="quantity" placeholder="Quantity" class="form-control" value="{{ old('quantity') }}">
                     </div>
+                    
                     <div class="form-group">
                         <label for="price">Price <span class="text-danger">*</span></label>
                         <div class="input-group">
-                            <span class="input-group-addon">PHP</span>
+                            <span class="input-group-addon">GBP</span>
                             <input type="text" name="price" id="price" placeholder="Price" class="form-control" value="{{ old('price') }}">
                         </div>
                     </div>
+                    
+                   <div class="form-group">
+                        <label for="cost_price">Cost Price <span class="text-danger">*</span></label>
+                        <div class="input-group">
+                            <span class="input-group-addon">GBP</span>
+                            <input type="text" name="cost_price" id="cost_price" placeholder="Cost Price" class="form-control" value="{{ old('cost_price') }}">
+                        </div>
+                    </div>
+                    
                     @if(!$brands->isEmpty())
                     <div class="form-group">
                         <label for="brand_id">Brand </label>
@@ -53,6 +69,7 @@
                         </select>
                     </div>
                     @endif
+                    
                     @if(!$channels->isEmpty())
                     <div class="form-group">
                         <label for="brand_id">Channels </label>
@@ -64,14 +81,17 @@
                         </select>
                     </div>
                     @endif
+                    
                     @include('admin.shared.status-select', ['status' => 0])
                     @include('admin.shared.attribute-select', [compact('default_weight')])
                 </div>
+                
                 <div class="col-md-4">
                     <h2>Categories</h2>
                     @include('admin.shared.categories', ['categories' => $categories, 'selectedIds' => []])
                 </div>
             </div>
+            
             <!-- /.box-body -->
             <div class="box-footer">
                 <div class="btn-group">
