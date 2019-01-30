1179ed8b479dae6229dfc0f351d3fccf7d103f69
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-30 11:24:07 +0000

    Update search.blade.php

--- resources/views/admin/products/search.blade.php
+++ resources/views/admin/products/search.blade.php
@@ -1,1 +1,13 @@
-
+      <div class="box">
+	            <div class="box-body">
+	                <h2>Products</h2>
+	                @if($products)
+	                @include('admin.shared.products')
+	                {{ $products->links() }}
+	                @endif
+	
+	            </div>
+	
+	
+	
+	        </div>
