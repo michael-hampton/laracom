4a158f066a9e618984bccb1acf5d71722cd3d50a
michael-hampton <michaelhamptondesign@yahoo.com>
2019-01-30 11:54:04 +0000

    Update list.blade.php

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -69,6 +69,26 @@
 <!-- /.content -->
 @endsection
 
+<div class="modal inmodal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content animated bounceInRight">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Edit Product</h4>
+            </div>
+
+            <div class="modal-body">
+
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-white" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary UpdateChannel">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @section('js')
 <script type="text/javascript">
     $(document).ready(function () {
