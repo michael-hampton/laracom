f8861ee5f13c91b8a868554246535980a463347a
michael-hampton <michaelhamptondesign@yahoo.com>
2019-02-02 14:54:19 +0000

    Create ChannelPriceImport.php

---
+++ app/Shop/ChannelPrices/ChannelPriceImport.php
@@ -0,0 +1,190 @@
+<?php
+namespace App\Shop\ChannelPrices;
+use App\Shop\Products\Repositories\ProductRepository;
+use App\Shop\Channels\Repositories\ChannelRepository;
+use App\Shop\Import\BaseImport;
+class ChannelPriceImport extends BaseImport {
+    protected $requiredFields = array(
+        'product',
+        'channel',
+        'price',
+        'warehouse'
+    );
+    /**
+     *
+     * @var type 
+     */
+    private $arrProducts = [];
+    
+    /**
+     *
+     * @var type 
+     */
+    private $arrWarehouses = [];
+    
+    /**
+     *
+     * @var type 
+     */
+    private $arrChannels = [];
+   
+    /**
+     *
+     * @var type 
+     */
+    private $productRepo;
+    /**
+     * 
+     * @param WarehouseRepository $warehouseRep
+     * @param ChannelRepository $channelRepo
+     * @param ProductRepository $productRepo
+     */
+    public function __construct(
+    WarehouseRepository $warehouseRepo, ChannelRepository $channelRepo, ProductRepository $productRepo
+    ) {
+        parent::__construct();
+        $this->productRepo = $productRepo;
+        //$this->arrWarehouses = array_change_key_case($categoryRepo->listCategories()->keyBy('name')->toArray(), CASE_LOWER);
+       
+        $this->arrChannels = array_change_key_case($channelRepo->listChannels()->keyBy('name')->toArray(), CASE_LOWER);
+        $this->arrProducts = array_change_key_case($productRepo->listProducts()->keyBy('name')->toArray(), CASE_LOWER);
+    }
+    /**
+     * 
+     * @param type $file
+     * @return boolean
+     */
+    private function importCsv($file) {
+        $handle = fopen($file, 'r');
+        if (!$handle) {
+            return false;
+        }
+        //Parse the first row, instantiate all the validators
+        $row = $this->parseFirstRow($this->fgetcsv($handle));
+        if (!empty($this->arrErrors)) {
+            return false;
+        }
+        while (($data = $this->fgetcsv($handle)) !== false) {
+            $order = array_map('trim', $this->mapData($data));
+            foreach ($order as $key => $params) {
+                $this->checkRule(['key' => $key, 'value' => $params]);
+            }
+            //$arrSelectedCategories = $this->validateCategories($order['categories']);
+            $this->validateChannel($order['channels']);
+           
+            $this->checkIfProductExists($order['name']);
+            
+            if (!empty($this->arrErrors)) {
+                return false;
+            }
+            
+            $this->buildProduct($order, $arrSelectedCategories, $arrSelectedChannels, $brand);
+        }
+        if (!$this->saveImport()) {
+            $this->arrErrors[] = 'Failed to save import';
+            return false;
+        }
+        fclose($handle);
+    }
+    /**
+     * 
+     * @param type $productName
+     * @return boolean
+     */
+    private function checkIfProductExists($productName) {
+        $productName = trim(strtolower($productName));
+        if (isset($this->arrProducts[$productName])) {
+            $this->arrErrors['product'] = 'The product you are trying to create already exists';
+            return true;
+        }
+        return false;
+    }
+    private function mapData($data) {
+        list(
+                $order['product'],
+                $order['channel'],
+                $order['price'],
+                $order['warehouse']
+                ) = $data;
+        return $order;
+    }
+    /**
+     * 
+     * @return boolean
+     */
+    private function saveImport() {
+        foreach ($this->arrProducts as $arrProduct) {
+            $arrCategories = $arrProduct['categories'];
+            $arrChannels = $arrProduct['channels'];
+            unset($arrProduct['categories']);
+            unset($arrProduct['channels']);
+            $arrProduct['slug'] = str_slug($arrProduct['name']);
+            $product = $this->productRepo->createProduct($arrProduct);
+            $productRepo = new ProductRepository($product);
+            // categories
+            if (!empty($arrCategories)) {
+                $productRepo->syncCategories($arrCategories);
+            }
+            // channels
+            if (!empty($arrChannels)) {
+                $productRepo->syncChannels($arrChannels);
+            }
+        }
+        return true;
+    }
+    /**
+     * Checks a CSV file for validity based on defined policies.
+     *
+     * Stops on the first violation
+     *
+     * @access public
+     * @param string $file Full path
+     * @return boolean
+     */
+    public function isValid($file) {
+        if (!file_exists($file)) {
+            $this->arrErrors[] = 'File ' . $file . ' does not exist.';
+            return false;
+        }
+        $this->importCsv($file);
+        return empty($this->arrErrors);
+    }
+    private function buildProduct($order, $arrSelectedCategories, $arrSelectedChannels, $brand) {
+        $this->arrProducts[] = [
+            'product' => $order['product'],
+            'price' => $order['price'],
+            'warehouse' => $order['warehouse'],
+            'channel' => $order['channel'],
+            'price' => $order['price']
+           
+        ];
+    }
+    /**
+     * 
+     * @param type $categories
+     * @return type
+     */
+    private function validateWarehouse($warehouse) {
+       if (!isset($this->arrWarehouses[$warehouse])) {
+                $this->arrErrors['warehouse'] = "Warehouse is invalid.";
+                return false;
+        }
+        
+        return true;
+        
+    }
+ 
+    private function validateChannel($channel) {
+      
+            if (!isset($this->arrChannels[$channel])) {
+                $this->arrErrors['channel'] = "Channel is invalid.";
+                return false;
+        }
+        return true;
+    }
+    
+    private function validateCostPrice($price) {
+      
+        return true;
+    }
+}
