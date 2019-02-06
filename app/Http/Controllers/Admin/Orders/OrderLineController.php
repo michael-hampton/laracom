df0a0509951af43677647f6d6ef03fbf00964410
michael-hampton <michael.hampton@kondor.co.uk>
2019-02-06 11:23:16 +0000

    test backorders

--- app/Http/Controllers/Admin/Orders/OrderLineController.php
+++ app/Http/Controllers/Admin/Orders/OrderLineController.php
@@ -31,6 +31,7 @@ use App\Traits\OrderCommentTrait;
 class OrderLineController extends Controller {
 
     use OrderCommentTrait;
+
     /**
      * @var OrderRepositoryInterface
      */
@@ -76,12 +77,14 @@ class OrderLineController extends Controller {
 
         $arrErrors = [];
 
-        foreach ($request->form as $arrData) {
+        foreach ($request->form as $arrData)
+        {
 
             $validator = Validator::make($arrData, (new UpdateLineRequest())->rules());
 
             // Validate the input and return correct response
-            if ($validator->fails()) {
+            if ($validator->fails())
+            {
                 return response()->json(['http_code' => 400, 'errors' => $validator->getMessageBag()->toArray()]);
             }
 
@@ -100,7 +103,8 @@ class OrderLineController extends Controller {
             }
         }
 
-        if (!empty($arrErrors)) {
+        if (!empty($arrErrors))
+        {
             return response()->json(['http_code' => 400, 'errors' => $arrErrors]);
         }
 
@@ -121,7 +125,8 @@ class OrderLineController extends Controller {
         $arrErrors = [];
         $arrSuccess = [];
 
-        foreach ($request->form as $arrLine) {
+        foreach ($request->form as $arrLine)
+        {
 
             try {
                 $orderProduct = $this->orderLineRepo->findOrderProductById($arrLine['line_id']);
@@ -147,12 +152,17 @@ class OrderLineController extends Controller {
             $comment = $orderProduct->product_name . ' changed to ' . $product->name;
             $this->saveComment($order, $comment);
         }
-        
+
         $arrResponse['details']['FAILURES'] = $arrErrors;
         $arrResponse['details']['SUCCESS'] = $arrSuccess;
         return response()->json($arrResponse);
     }
 
+    /**
+     * 
+     * @param Request $request
+     * @return type
+     */
     public function search(Request $request) {
 
         $orders = $this->orderRepo->listOrders('is_priority', 'desc')->keyBy('id');
@@ -170,8 +180,8 @@ class OrderLineController extends Controller {
         $items = $this->orderLineRepo->paginateArrayResults($list, 10);
 
         return view('admin.order-lines.search', [
-            'items' => $items,
-            'orders' => $orders,
+            'items'    => $items,
+            'orders'   => $orders,
             'products' => $arrProducts
                 ]
         );
@@ -194,10 +204,16 @@ class OrderLineController extends Controller {
                 })->all();
     }
 
+    /**
+     * 
+     * @param int $digits
+     * @return type
+     */
     private function generatePIN(int $digits = 4) {
         $i = 0; //counter
         $pin = ""; //our default pin is blank.
-        while ($i < $digits) {
+        while ($i < $digits)
+        {
             //generate a random number between 0 and 9.
             $pin .= mt_rand(0, 9);
             $i++;
@@ -212,15 +228,19 @@ class OrderLineController extends Controller {
     public function doAllocation(Request $request) {
 
         $os = $this->orderStatusRepo->findByName('Waiting Allocation');
-        
+        $picklistRef = $this->generatePIN();
+
         $arrDone = [];
         $arrFailed = [];
         $blError = false;
 
-        foreach ($request->lineIds as $orderId => $arrLines) {
+        foreach ($request->lineIds as $orderId => $arrLines)
+        {
 
-            foreach ($arrLines as $lineId) {
-                if (in_array($orderId, $arrDone)) {
+            foreach ($arrLines as $lineId)
+            {
+                if (in_array($orderId, $arrDone))
+                {
 
                     continue;
                 }
@@ -230,43 +250,53 @@ class OrderLineController extends Controller {
                 $statusCount = $this->orderLineRepo->chekIfAllLineStatusesAreEqual($order, $os->id);
                 $arrProducts = $this->orderLineRepo->listOrderProducts()->where('order_id', $order->id);
 
-                if ($statusCount === 0) {
+                if ($statusCount === 0)
+                {
 
-                    foreach ($arrProducts as $objLine) {
+                    foreach ($arrProducts as $objLine)
+                    {
 
-                        if ($objLine->status !== $os->id) {
+                        if ($objLine->status !== $os->id)
+                        {
 
                             continue;
                         }
 
-                        if ($channel->allocate_on_order === 0 || $order->payment === 'import') {
+                        if ($channel->allocate_on_order === 0 || $order->payment === 'import')
+                        {
                             // check enough quantity to fulfil line if not reject
                             // update stock
-                           
-                            if(!$this->increaseReservedStock($objLine, false)) {
+
+                            if (!$this->increaseReservedStock($objLine, false))
+                            {
                                 $arrFailed[$lineId][] = 'failed to update stock';
                             }
-                            
                         }
 
-                         if(!$this->addToPicklist($lineId, $order)) {
-                        $arrFailed[$lineId][] = 'Unable to add picklist';
+                        if (!$this->addToPicklist($lineId, $picklistRef, $order))
+                        {
+                            $arrFailed[$lineId][] = 'Unable to add picklist';
+                        }
                     }
-                    }
-                } elseif ($channel->partial_shipment === 1) {
+                }
+                elseif ($channel->partial_shipment === 1)
+                {
 
                     $objLine = $this->orderLineRepo->findOrderProductById($lineId);
 
-                    if ($channel->allocate_on_order === 1 || $order->payment === 'import') {
-                        
+                    if ($channel->allocate_on_order === 1 || $order->payment === 'import')
+                    {
+
                         //$quantity = $objProduct->quantity - $objLine2->quantity;
 
-                         if(!$this->increaseReservedStock($objLine)) {
-                                $arrFailed[$lineId][] = 'failed to update stock';
-                            }
+                        if (!$this->increaseReservedStock($objLine))
+                        {
+                            $arrFailed[$lineId][] = 'failed to update stock';
+                        }
                     }
 
-                    if(!$this->addToPicklist($lineId)) {
+                    if (!$this->addToPicklist($lineId, $picklistRef))
+                    {
                         $arrFailed[$lineId][] = 'Unable to add picklist';
                     }
                 }
@@ -278,76 +308,97 @@ class OrderLineController extends Controller {
         $http_code = $blError === true ? 400 : 200;
         return response()->json(['http_code' => $http_code, 'FAILURES' => $arrFailed, 'SUCCESS' => $arrDone]);
     }
-    
-    private function addToPicklist($lineId, $order = null) {
+
+    /**
+     * 
+     * @param type $lineId
+     * @param type $picklistRef
+     * @param type $order
+     * @return boolean
+     */
+    private function addToPicklist($lineId, $picklistRef, $order = null) {
         try {
-            $picklistRef = $this->generatePIN();
+
             $objNewStatus = $this->orderStatusRepo->findByName('ordered');
-                        // update line status
-                        $orderLineRepo = new OrderProductRepository(new OrderProduct);
-                        $orderLineRepo->update(['status' => $objNewStatus->id, 'picklist_ref' => $picklistRef], $lineId);
-            
-            if($order !== null) {
+            // update line status
+            $orderLineRepo = new OrderProductRepository(new OrderProduct);
+            $orderLineRepo->update(['status' => $objNewStatus->id, 'picklist_ref' => $picklistRef], $lineId);
+
+            if ($order !== null)
+            {
                 $order->order_status_id = $objNewStatus->id;
                 $order->save();
             }
-                    } catch (\Exception $e) {
-                        return false;
-                    }
-        
+        } catch (\Exception $e) {
+            return false;
+        }
+
         return true;
     }
-    
+
+    /**
+     * 
+     * @param type $objLine
+     * @param type $order
+     * @return boolean
+     */
     private function reserveStock($objLine, $order = null) {
         try {
             $objProduct = $this->productRepo->findProductById($objLine->product_id);
 
             $availiableQty = $objProduct->quantity - $objProduct->reserved_stock;
 
-            if($availiableQty < $objLine->quantity) {
+            if ($availiableQty < $objLine->quantity)
+            {
                 return false;
             }
-                    
-            $objNewStatus = $this->orderStatusRepo->findByName('Waiting Allocation');
-            $reserved_stock = $objProduct->reserved_stock - $objLine->quantity;
-                                //$quantity = $objProduct->quantity - $objLine2->quantity;
-
-                                $objProductRepo = new ProductRepository($objProduct);
-                                $objProductRepo->updateProduct(['reserved_stock' => $reserved_stock]);
-
-                                $objLine->status = $objNewStatus->id;
-                                $objLine->save();
             
-            if($order !== null) {
+            $objNewStatus = $this->orderStatusRepo->findByName('Waiting Allocation');
+            $reserved_stock = $objProduct->reserved_stock + $objLine->quantity;
+            
+            $objProductRepo = new ProductRepository($objProduct);
+            $objProductRepo->updateProduct(['reserved_stock' => $reserved_stock]);
+
+            $orderLineRepo = new OrderProductRepository($objLine);
+            $orderLineRepo->updateOrderProduct(['status' => $objNewStatus->id]);
+
+            if ($order !== null)
+            {
                 $order->order_status_id = $objNewStatus->id;
-               $order->save();
+                $order->save();
             }
-                            } catch (\Exception $e) {
-                                
-                                return false;
-                            }
-        
+        } catch (\Exception $e) {
+
+            return false;
+        }
+
         return true;
     }
-    
+
+    /**
+     * 
+     * @param type $objLine
+     * @param type $blAllowPartial
+     * @return boolean
+     */
     private function increaseReservedStock($objLine, $blAllowPartial = true) {
         try {
             $objProduct = $this->productRepo->findProductById($objLine->product_id);
-            
+
             $quantity = $objProduct->quantity - $objProduct->reserved_stock;
-                              
-            if($blAllowPartial === false && $objLine->quantity < $quantity) {
-                return false;             
+
+            if ($blAllowPartial === false && $objLine->quantity < $quantity)
+            {
+                return false;
             }
-            
+
             $reserved_stock = $objProduct->reserved_stock + $objLine->quantity;
-                                $objProductRepo = new ProductRepository($objProduct);
-                                $objProductRepo->updateProduct(['reserved_stock' => $reserved_stock]);
-                            } catch (\Exception $e) {
-                                return false;
-                                
-                            }
-        
+            $objProductRepo = new ProductRepository($objProduct);
+            $objProductRepo->updateProduct(['reserved_stock' => $reserved_stock]);
+        } catch (\Exception $e) {
+            return false;
+        }
+
         return true;
     }
 
@@ -359,15 +410,17 @@ class OrderLineController extends Controller {
 
         $productRepo = new ProductRepository(new Product);
         $os = $this->orderStatusRepo->findByName('Backorder');
-p
         $arrDone = [];
         $arrFailed = [];
         $blError = false;
 
-        foreach ($request->lineIds as $orderId => $arrLines) {
+        foreach ($request->lineIds as $orderId => $arrLines)
+        {
 
-            foreach ($arrLines as $lineId) {
-                if (in_array($orderId, $arrDone)) {
+            foreach ($arrLines as $lineId)
+            {
+                if (in_array($orderId, $arrDone))
+                {
 
                     continue;
                 }
@@ -382,59 +435,70 @@ p
                 $backorderCount = 0;
                 $intCantMove = 0;
 
-                if ($total === 0) {
+                if ($total === 0)
+                {
                     continue;
                 }
 
                 // work out how many lines can be moved
 
-                foreach ($arrProducts as $objProductLine) {
+                foreach ($arrProducts as $objProductLine)
+                {
 
                     $product = $productRepo->findProductById($objProductLine->product_id);
 
                     $availiableQty = $product->quantity - $product->reserved_stock;
 
-                    if ($objProductLine->status === $os->id && $availiableQty <= $objProductLine->quantity) {
+                    if ($objProductLine->status === $os->id && $availiableQty <= $objProductLine->quantity)
+                    {
 
                         $intCantMove++;
                     }
 
-                    if ($objProductLine->status === $os->id) {
+                    if ($objProductLine->status === $os->id)
+                    {
                         $backorderCount++;
                     }
                 }
 
 
-                if ($total > $backorderCount && $channel->partial_shipment === 0) {
+                if ($total > $backorderCount && $channel->partial_shipment === 0)
+                {
 
                     // cant complete because there are more than 1 line that are backordered and no partial shipping allowed
                     $arrFailed[$lineId][] = 'Unable to move';
                     $blError = true;
 
                     // if partial shipping allowed and more than 1 line backordered then move single line
-                } elseif ($intCantMove === 0 && $backorderCount > 1) {
+                }
+                elseif ($intCantMove === 0 && $backorderCount > 1)
+                {
 
-                    foreach ($arrProducts as $objLine2) {
-                        
-                        if(!$this->reserveStock($objLine2, $order)){
+                    foreach ($arrProducts as $objLine2)
+                    {
+
+                        if (!$this->reserveStock($objLine2, $order))
+                        {
                             $arrFailed[$lineId][] = 'failed to allocate stock';
                         }
                     }
-                
-                } elseif (($backorderCount === $total && $backorderCount === 1) || $channel->partial_shipment === 1) {
-
+                }
+                elseif (($backorderCount === $total && $backorderCount === 1) || $channel->partial_shipment === 1)
+                {
+                    
                     $objLine2 = $this->orderLineRepo->findOrderProductById($lineId);
-                        
+
                     $updateOrder = null;
 
-                    if ($total === 1 && $backorderCount === 1) {
+                    if ($total === 1 && $backorderCount === 1)
+                    {                        
                         $updateOrder = $order;
                     }
                     
-                    if(!$this->reserveStock($objLine2, $updateOrder)){
+                    if (!$this->reserveStock($objLine2, $updateOrder))
+                    {
                         $arrFailed[$lineId][] = 'failed to allocate stock';
                     }
-                    
                 }
 
                 $arrDone[$lineId] = "Order {$orderId} Line {$lineId} was updated successfully";
