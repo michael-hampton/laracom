bbe2172b8a4970ec09ae5bb89906015b229417ba
michael-hampton <michaelhamptondesign@yahoo.com>
2019-02-06 09:52:29 +0000

    Update OrderCommentTrait.php

--- app/Traits/OrderCommentTrait.php
+++ app/Traits/OrderCommentTrait.php
@@ -1,1 +1,14 @@
 
+namespace App\Traits;
+use App\Shop\Orders\Order;
+/**
+ * Description of OrderCommentTrait
+ *
+ * @author michael.hampton
+ */
+trait OrderCommentTrait {
+
+public function saveComment(Order $order) {
+
+}
+}
